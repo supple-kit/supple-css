018fe9f5baf37dc5f637b44a8677875ff5e886d1
Bram Smulders <bram@bramsmulders.com>
2017-10-31 20:52:27 +0100

    Added postcss-custom-properties to test preservation

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   plugins: [
-    require('postcss-easy-import'),
-    require('postcss-color-function'),
-    require('postcss-apply'),
-    require('postcss-custom-media'),
-    require('autoprefixer'),
-    require('cssnano')({
-      preset: 'default',
-    })
-  ]
-}
+    require("postcss-easy-import"),
+    require("postcss-color-function"),
+    require("postcss-apply"),
+    require("postcss-custom-media"),
+    // require("postcss-custom-properties")({
+    //   preserve: true,
+    // }),
+    require("autoprefixer"),
+    require("cssnano")({
+      preset: "default",
+    }),
+  ],
+};
