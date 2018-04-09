c85ac39eb605bedbc0fa0f1d71d1ff92d323c241
Bram Smulders <bram@bramsmulders.com>
2018-04-09 21:48:40 +0200

    Don't rely on the apply rule anymore since it's an abandoned spec

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: [
     require("postcss-easy-import"),
-    require("postcss-color-function"),
-    require("postcss-apply"),
     // require("postcss-custom-properties")({
     //   preserve: true
     // }),
