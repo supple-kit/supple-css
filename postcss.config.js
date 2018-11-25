b784311709b0480fa3b2e8585b74e74323b47ab5
Bram Smulders <bram@bramsmulders.com>
2018-11-25 21:56:45 +0100

    Remove unused packages

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   plugins: [
-    // require("postcss-easy-import"),
-    // require("postcss-custom-properties")({
-    //   preserve: true
-    // }),
-    require("postcss-custom-media"),
     // require("autoprefixer"),
     require("cssnano")({
       preset: "default",
